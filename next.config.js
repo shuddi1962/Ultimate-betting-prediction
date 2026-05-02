ebd995883a5a50b99583a87ad0547f799e55bc9a
Roshanal Admin <admin@roshanalglobal.com>
2026-05-02 07:59:04 +0100

    Add webpack alias for @/ in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
   },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname);
+    return config;
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
